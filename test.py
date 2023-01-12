8de278b6a7bd735e9d37a76ccbcce6de16edcd83
Zeid Solh <solh@hcx.us>
2023-01-12 15:16:39 -0800

    Added test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+	x = "ECE_180_DA_DB"
+	if x == "EE_180_DA_DB":
+		print("You are living in 2017")
+	else:
+		# this is a comment
+		x = x + " - Best class ever"
+		print(x)
