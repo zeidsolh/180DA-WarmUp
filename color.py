684d06d6e203ed78a81c58e5e2cb686ee3b8262d
Zeid Solh <solh@hcx.us>
2023-01-18 14:14:59 -0800

    Added color.py which detects blue objects

---
+++ color.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2  
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    # Convert BGR to HSV
+    # hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # define range of blue color in HSV
+    lower_blue = np.array([200,0,0])
+    upper_blue = np.array([255,40,40])
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange (frame, lower_blue, upper_blue)
+    bluecnts = cv2.findContours(mask.copy(),
+                              cv2.RETR_EXTERNAL,
+                              cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+    if len(bluecnts)>0:
+        blue_area = max(bluecnts, key=cv2.contourArea)
+        (xg,yg,wg,hg) = cv2.boundingRect(blue_area)
+        cv2.rectangle(frame,(xg,yg),(xg+wg, yg+hg),(0,255,0),2)
+
+    cv2.imshow('frame',frame)
+
+    k = cv2.waitKey(5) 
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
